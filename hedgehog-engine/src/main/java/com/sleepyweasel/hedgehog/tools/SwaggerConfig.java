e7648c00319e3faa04ae6839662ad95483df003a
Karol Katerzawa <karol.katerzawa@syncron.com>
2018-09-22 19:47:55 +0200

    Add Swagger config

---
+++ hedgehog-engine/src/main/java/com/sleepyweasel/hedgehog/tools/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.sleepyweasel.hedgehog.tools;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+class SwaggerConfig extends WebMvcConfigurationSupport {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+        .pathMapping("/")
+        .apiInfo(metaData());
+  }
+
+  private ApiInfo metaData() {
+    return new ApiInfoBuilder()
+        .title("EveryYear")
+        .version("0.0.1-SNAPSHOT")
+        .license("MIT")
+        .licenseUrl("https://opensource.org/licenses/MIT")
+        .contact(
+            new Contact("Sleepy Weasel", "https://github.com/OperationSleepyWeasel/hedgehog", ""))
+        .build();
+  }
+
+  @Override
+  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
+}
